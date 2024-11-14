d24f0d90ccd2d1c11ded9b7eca0ed40b005e6718
Abner2646 <abnergrgurich@gmail.com>
2024-11-13 22:44:18 -0300

    .

---
+++ Python/Tp8/Ej3.py
@@ -0,0 +1,22 @@
+'''Una agencia de viajes especializada en destinos turísticos de Argentina necesita un sistema
+de gestión de viajes para organizar y ofrecer paquetes turísticos grupales a sus clientes. La
+agencia requiere almacenar, actualizar y visualizar la información de cada destino, el tipo de
+viaje, el alojamiento y el medio de transporte para cada paquete turístico.
+Requerimientos:
+1. Paquete grupal: Cada paquete de viaje grupal tiene una ciudad, fechas de ida y vuelta, una
+descripción del viaje, un tipo de viaje (entre las opciones: turismo, educativo o aventura), y
+un precio. También se debe mantener información sobre el cupo máximo y el cupo actual de
+personas en cada paquete grupal. Cada paquete tiene un número que lo identifica.
+2. Ciudad: Cada paquete de viaje tiene una ciudad, la ciudad tiene un nombre, la provincia
+donde se ubica, y los puntos turísticos a donde se puede ir a pasear. Cada ciudad se puede
+Programación II Universidad Tecnológica Nacional
+Tecnicatura Universitaria en Programación Segundo cuatrimestre 2024
+identificar mediante el nombre y la provincia, ya que puede haber ciudades con el mismo
+nombre en distintas provincias.
+3. Hotel: Cada paquete incluye un alojamiento que ofrece información sobre el nombre del
+hotel, la ciudad donde se ubica, una breve descripción, la categoría de estrellas del
+alojamiento y el tipo de pensión (entre las opciones: desayuno, media pensión, pensión
+completa). Los hoteles se identifican mediante el nombre y la ciudad donde se encuentran
+ubicados.
+4. Transporte: Los paquetes turísticos incluyen el tipo de transporte que se utilizará (por
+ejemplo, avión, colectivo) para ir y volver del destino turístico. '''
